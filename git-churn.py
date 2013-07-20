9e3b917c6c5092325c6d5e2dcaed21815a3c0d78
Feng Qiu <feng@ban90.com>
2013-07-20 15:34:32 +0800

    Use git-log instead of git-diff-tree to get diffstats
    A single git-log call is much faster than a git-diff-tree for every
    commit.
    
    Also improved the format of the result printout, to better match
    Git's idioms.

--- git-churn.py
+++ git-churn.py
@@ -6,9 +6,72 @@ import subprocess
 # already processed.
 processed = {}
 
+# Each key is the SHA-1 hash of a commit. The value is the parent(s)
+# of the commit.
+parents_cache = {}
+# Each key is the SHA-1 hash of a commit. The value is the diffstats
+# of the commit (compared to its parent).
+diffstats_cache = {}
+
+# Repeatedly calling git-diff-tree to get the diffstats works but is
+# too slow. So we have to use the "porcelain" git-log command and parse
+# its output. This function does just that and stores the diffstats
+# in a dictionary to be used later by process_commit().
+def get_diffstats():
+	logs = subprocess.check_output(
+		'git log --numstat --format="format:%H%n%P"', shell=True).splitlines()
+	
+	# Parse a single commit from the logs, starting at line ln
+	# Returns the starting line of the next commit, -1 if done.
+	def parse_commit(ln):
+		parents = []
+		stats = []
+
+		try:
+			# First line is the hash of this commit
+			commit = logs[ln]
+	
+			# Second line is the hash of its parents
+			parents = logs[ln+1].split()
+			parents_cache[commit] = parents
+	
+			# The third line is either the first line of this commit's
+			# diffstats or the hash of the next commit (its parent).
+			x = 2
+			items = logs[ln+x].strip().split('\t')
+			while len(items) > 1:
+				try:
+					stats.append((items[2], int(items[0]), int(items[1])))
+				except ValueError:
+					# This is a binary file, ignore it
+					pass
+				
+				x += 1
+				items = logs[ln+x].strip().split('\t')
+				
+			# Diffstats end with an empty line, jump past that
+			if items[0] == '':
+				x += 1
+	
+			# We have finished parsing the diffstats, job for this commit
+			# is done.
+			diffstats_cache[commit] = stats
+			
+			return ln + x
+
+		except IndexError:
+			# The parents line doesn't exist, this must be the root commit.
+			parents_cache[commit] = parents
+			diffstats_cache[commit] = stats
+			return -1
+
+	ln = 0
+	while ln >= 0:
+		ln = parse_commit(ln)
+
 
 # This function takes the SHA-1 hash of a commit and returns a list of
-# file paths and corresponding addition and deletion line counts, sorted
+# file paths and corresponding insertion and deletion line counts, sorted
 # alphabetically by file paths, according to changes done by this commit
 # AND all its ancestors.
 #
@@ -21,8 +84,7 @@ def process_commit(commit):
 		processed[commit] = True
 
 		# First get a list of parent hashes of the commit
-		parents = subprocess.check_output(
-			'git show --summary --pretty="%P" ' + commit, shell=True).splitlines()[0].split()
+		parents = parents_cache[commit]
 
 		# This array contains a list of result arrays to be merged.
 		results = []
@@ -44,44 +106,20 @@ def process_commit(commit):
 			if len(parent_result) > 0:
 				results.append(parent_result)
 			
-			# Now do a diff between parent and commit to get the changes in this
-			# commit
-			diff = []
-			stats = subprocess.check_output(
-				'git diff-tree --numstat --no-commit-id -r %s %s' % (parent, commit), shell=True)
-			stats = stats.splitlines()
-			for stat in stats:
-				items = stat.strip().split('\t')
-				try:
-					diff.append((items[2], int(items[0]), int(items[1])))
-				except ValueError:
-					# This is a binary file
-					pass
-			
-			if len(diff) > 0:
-				results.append(diff)
+			# Now get the diffstats of the commit (against its parent)
+			stats = diffstats_cache[commit]
+			if len(stats) > 0:
+				results.append(stats)
 		
 		else:
 			# This is a root commit
-			diff = []
-			stats = subprocess.check_output(
-				'git diff-tree --numstat --no-commit-id --root -r %s' % commit, shell=True)
-			stats = stats.splitlines()
-			for stat in stats:
-				items = stat.strip().split('\t')
-				try:
-					diff.append((items[2], int(items[0]), int(items[1])))
-				except ValueError:
-					# This is a binary file
-					pass
-
-			return diff
+			return diffstats_cache[commit]
 		
 		# Finally merge result arrays in results to get the result for this
 		# commit.
 		#
 		# Note that we don't currently try to detect renames. So a rename
-		# will cause N deletions and N additions, in total 2N line changes.
+		# will cause N deletions and N insertions, in total 2N line changes.
 		# It also seems like "hg churn" works the same way.
 		#
 		# Also note that we utilize the fact that git sorts the output of
@@ -104,21 +142,21 @@ def process_commit(commit):
 				
 			while True:
 				# Do an N-way merge 
-				(path, additions, deletions) = results[0][indices[0]]
+				(path, insertions, deletions) = results[0][indices[0]]
 				chosen = [0]
 				for i in range(1, N):
 					if results[i][indices[i]][0] < path:
 						# This path is smaller, take this
-						(path, additions, deletions) = results[i][indices[i]]
+						(path, insertions, deletions) = results[i][indices[i]]
 						chosen = [i]
 					elif results[i][indices[i]][0] == path:
 						# This is the same path, merge the line counts
-						additions += results[i][indices[i]][1]
+						insertions += results[i][indices[i]][1]
 						deletions += results[i][indices[i]][2]
 						chosen.append(i)
 
 				# Add the new path into the result list
-				retval.append((path, additions, deletions))
+				retval.append((path, insertions, deletions))
 
 				# Finally increment the indices of the chosen arrays
 				for i in range(len(chosen)-1, -1, -1):
@@ -146,16 +184,31 @@ def process_commit(commit):
 head = subprocess.check_output('git rev-parse HEAD', shell=True).strip()
 
 # Now process HEAD
+get_diffstats()
 result = process_commit(head)
 
 # Print the result
-additions = 0
+changed = len(result)
+insertions = 0
 deletions = 0
-for item in result:
-	additions += item[1]
-	deletions += item[2]
-	print '%s\t%s\t%s' % (item[1], item[2], item[0])
-print '\n%s lines added, %s lines deleted.\n' % (additions, deletions)
+
+if changed > 0:
+	for item in result:
+		insertions += item[1]
+		deletions += item[2]
+		print '%s\t%s\t%s' % (item[1], item[2], item[0])
+
+	changed_text = '%s file%s changed' % (changed, changed>1 and 's' or '')
+	if insertions > 0:
+		insertions_text = ', %s insertion%s(+)' % (insertions, insertions>1 and 's' or '')
+	else:
+		insertions_text = ''
+	if deletions > 0:
+		deletions_text = ', %s deletion%s(-)' % (deletions, deletions>1 and 's' or '')
+	else:
+		deletions_text = ''
+
+	print '\n%s%s%s\n' % (changed_text, insertions_text, deletions_text)
 
 
 
